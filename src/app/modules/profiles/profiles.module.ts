254ab0101bedae4dc392feda216d84bce5d2a8f4
markovsz <58207528+markovsz@users.noreply.github.com>
2023-05-18 18:10:28 +0300

    add profiles components to module

--- src/app/modules/profiles/profiles.module.ts
+++ src/app/modules/profiles/profiles.module.ts
@@ -3,12 +3,20 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ViewDoctorsComponent } from './doctors/components/view-doctors/view-doctors.component';
+import { ViewPatientsComponent } from './patients/components/view-patients/view-patients.component';
+import { ViewPatientProfileComponent } from './patients/components/view-patient-profile/view-patient-profile.component';
+import { ViewProfileComponent } from './profile/view-profile/view-profile.component';
+import { ViewDoctorProfileComponent } from './doctors/components/view-doctor-profile/view-doctor-profile.component';
 
 
 
 @NgModule({
   declarations: [
-    ViewDoctorsComponent
+    ViewDoctorsComponent,
+    ViewPatientsComponent,
+    ViewPatientProfileComponent,
+    ViewProfileComponent,
+    ViewDoctorProfileComponent
   ],
   imports: [
     CommonModule,
@@ -17,7 +25,8 @@ import { ViewDoctorsComponent } from './doctors/components/view-doctors/view-doc
     ReactiveFormsModule
   ],
   exports: [
-    ViewDoctorsComponent
+    ViewDoctorsComponent,
+    ViewProfileComponent
   ]
 })
 export class ProfilesModule { }
